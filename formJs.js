a6733d1ff56dc229cc9c426bd0f6335fe59cb22f
Rajmfis <rajy@mindfiresolutions.com>
2020-01-07 15:42:35 +0530

    Repetitive used DOM elements catched in variables, function headers and inline comments are added

--- formJs.js
+++ formJs.js
@@ -1,21 +1,33 @@
-var globalVariable = 1;
+//id of everyuser, keeps varying on deletion  
+var id = 1;
 var bool;
-var city;
-var todelete;
-var userId=[];
-function validate() {
+var city;//picking the city input from the dropdown
+var todelete;//holds the row to be deleted
+
+// the below selectors are the div tag selectors
+const divfname=$('#firstName');
+const divlname=$('#lastName');
+var divmail=$('#Email');
+const divcontact=$('#Contact');
+const divpwd=$('#Password');
+
+//the below selectors are the input fields selectors
+const inputfname=$('#firstname');
+const inputlname=$('#lastname');
+const inputemailid=$('#emailId');
+const inputcontactno=$('#contactno');
+
+//validate is called on submitting the form
+function validate(){//the function validates all the input fields 
 	
-	// e.preventDefault();
-	if(globalVariable===1){
+	if(id===1){
 		$('#usercreate').text('Please create new users');
-		// document.getElementById('usercreate').innerText='Please create new users';
 	}
 	var inputEmail = document.myform.email.value;//picking the email field value   
 	var phone = document.myform.contact.value;
 	var firstname = document.myform.firstName.value;
 	var lastname = document.myform.lastName.value;
 	var password = document.myform.password.value;
-	// alert(firstname);
 	var allValue = true;
 	var regexEmailPattern = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,10}).([a-z]{2,10}?)$/;
 	var regexPhonePattern = /^([0-9]{10,11})$/;
@@ -24,28 +36,28 @@ function validate() {
 
 	if (firstname === "") {
 		// alert("hi js tested");
-		$('#firstName').html('Please fill the firstName field');
-		$('#firstName').css('color', 'red');
+		divfname.html('Please fill the firstName field');
+		divfname.css('color', 'red');
 		allValue = false;
 	}
 	if (lastname === "") {
-		$('#lastName').html('Please fill the lastName field');
-		$('#lastName').css('color','red');
+		divlname.html('Please fill the lastName field');
+		divlname.css('color','red');
 		allValue = false;
 	}
 	if (inputEmail === "") {
-		$('#Email').html('Please fill the inputEmail field');
-		$('#Email').css('color','red');
+		divmail.html('Please fill the inputEmail field');
+		divmail.css('color','red');
 		allValue = false;
 	}
 	if (phone === "") {
-		$('#Contact').html('Please fill the phone field');
-		$('#Contact').css('color','red');
+		divcontact.html('Please fill the phone field');
+		divcontact.css('color','red');
 		allValue = false;
 	}
 	if (password === "") {
-		$('#Password').html('Please fill the password field');
-		$('#Password').css('color','red');
+		divpwd.html('Please fill the password field');
+		divpwd.css('color','red');
 		allValue = false;
 	}
 	if (allValue === false) {
@@ -53,41 +65,42 @@ function validate() {
 	}
 	if (checkPhone && allValue) {
 		if (checkEmail) {
-			$('#Password').text('');
-			$('#Email').text('');
-			$('#lastName').text('');
-			$('#firstName').text('');
-			$('#Contact').text('');
+			divpwd.text('');
+			divmail.text('');
+			divlname.text('');
+			divfname.text('');
+			divcontact.text('');
 			createTable(firstname, lastname, inputEmail, phone);
 			resetForm();
 			return false;
 		}
 	}
 	if (checkEmail === false && checkPhone === false) {
-		$('#Contact').html('Entered Contact no is wrong');
-		$('#Email').html('Entered Email id is wrong ');
-		$('#Contact').css('color', 'red');
-		$('#Email').css('color', 'red');
+		divcontact.html('Entered Contact no is wrong');
+		divmail.html('Entered Email id is wrong ');
+		divcontact.css('color', 'red');
+		divmail.css('color', 'red');
 		return false;
 	} else if (checkEmail === true && checkPhone === false) {
-		$('#Contact').html('Entered contact no is wrong');
-		$('#Contact').css('color','red');
+		divcontact.html('Entered contact no is wrong');
+		divcontact.css('color','red');
 		return false;
 	} else if (checkEmail === false && checkPhone === true) {
-		$('#Email').html('Entered email is incorrect');
-		$('#Email').css('color', 'red');
+		divmail.html('Entered email is incorrect');
+		divmail.css('color', 'red');
 		return false;
 	}
 }
 
 $('#colorpick').change(function () {
-	$('body').css('background-color') = $('#colorpick').val();
+	$('body').css('background-color',$('#colorpick').val());
 });
-
-function resetForm() {
-	$('#firstname').val('');
-	$('#lastname').val('');
-	$('#emailId').val('');
+//resetform is called for clearing the inputfields
+function resetForm() {//the method is called from validate, when all the records are submitted and 
+											//put in the table
+	inputfname.val('');
+	inputlname.val('');
+	inputemailid.val('');
 	$('#contactno').val('');
 	$('#pwd').val('');
 	$('#dropdown').val('');
@@ -99,52 +112,32 @@ function resetForm() {
 	$('#emailmessage').text('');
 	$('#contactmessage').text('');
 }
-
+//called from validate method for creating the table
 function createTable(firstname, lastname, inputEmail, phone) {
-	
+	//it is used to create table row and columns everytime when the user submits the details in the form
 	var mytable = $('table');
-	
+	//the block checks if there is any row to delete
 	if(bool===true && todelete!==undefined){
-		var mytable=$('table');
-		mytable.deleteRow(todelete);
+		$(this).closest("tr").remove();
 		bool=false;
 		todelete=undefined;
 	}
-	// alert("in createtable");
-	// var row = mytable.insertRow(globalVariable++);
-	// var cell1 = row.insertCell(0);
-	// var cell2 = row.insertCell(1);
-	// var cell3 = row.insertCell(2);
-	// var cell4 = row.insertCell(3);
-	// var cell5 = row.insertCell(4);
-	// var cell6 = row.insertCell(5);
-	// var cell7 = row.insertCell(6);
-	// cell1.innerHTML = globalVariable - 1;
-	// cell2.innerHTML = firstname;
-	// cell3.innerHTML = lastname;
-	// cell4.innerHTML = inputEmail;
-	// cell5.innerHTML = phone;
-	// cell6.innerHTML = '<button onclick="deleteUser(this);">&#10005</button> <button onclick="editUser(this);">Edit</button>';
-	
-	// cell6.setAttribute("onclick", "deleteUser(this);");//setattribute worked here for delete one
-	// cell7.innerHTML = '<button>Edit</button>';
-	// cell7.setAttribute("onclick", "editUser(this);");
-	// cell7.innerText = city;
-	var row='<tr id=\'data\'><td>'+(globalVariable)+'</td><td>'+
+	var row='<tr id=\'data\'><td>'+(id)+'</td><td>'+
 					firstname+'</td><td>'+lastname+'</td><td>'+inputEmail+'</td><td>'+phone+'</td><td>'+'<button onclick="deleteUser(this);">&#10005</button><button onclick="editUser(this);">Edit</button>'+'</td><td>'+city+'</td></tr>';
-	mytable.append(row);
+	mytable.append(row);//append method is used with jquery for table
 	$('#submit').val('Submit');
 	$('#usercreate').text('');
-	globalVariable++;
+	id++;
 }
-
+//called when the user clicks the delete button, it deletes the user from the table
 function deleteUser(element) {
+	//dialogbox asks whether the user wants to continue or not
 	var val = confirm("Are you sure you want to delete the user ?");
 	if (val === true) {
-		// alert('deleting the user'+element.parentNode.rowIndex);
-		$('#data').remove();
-		// globalVariable--;
-		if(globalVariable===1){
+		alert('deleting the user'+element.parentNode.parentNode.rowIndex);
+		$('table tr:eq('+element.parentNode.parentNode.rowIndex+')').remove();
+		id--;
+		if(id===1){
 			$('#usercreate').text('Please create new users');
 		}
 	}
@@ -152,49 +145,50 @@ function deleteUser(element) {
 		alert('User not deleted');
 	}
 }
-
+//editUser accepts the current object passed as this which will be used 
+//to track current user
 function editUser(user) {
-
+	debugger;
 	if(document.myform.firstName.value!=='' && document.myform.lastName.value!==''){
 		var clear=confirm('Your entered will be cleared. Do you want to continue');
 		if(clear===false){//scenario when form is dirty, so if it doesn't want to override values we simply return
-			// document.getElementById('dropdown').value = "";
-			// document.getElementById('states').remove();
-			// document.getElementById('cityname').remove();
 			return ;
 		}
 		if(clear==true){//if user wants to override and edit the values then there must be dropdowns so we remove it
 			//reason to put here  as if the below code is put in the code then there will be a problem for select tag as select
 			//tags and options are not yet created for the user and hence it wouldn't work. 
 			$('#dropdown').val('');
-			// document.getElementById('states').value = "";
-			// document.getElementById('cityname').value = "";
 			$('#states').remove();
 			$('#cityname').remove();
 		}
 	}
 
+	
+	var currentRow=$(this).closest("tr"); 
+	var col1=currentRow.find("table td:eq(0)").text(); 						// get current row 1st  cell TD value
+	var col2=$(this).find("td:eq(1)").text(); // get current row 2nd  cell TD value
+	var col3=$(this).parents("tr").find("td:eq(2)").text(); // get current row 3rd  cell  TD value
+	var col4=$(this).parents("tr").find("td:eq(3)").text(); // get current row 4th  cell TD value
+	var col5=$(this).parents("tr").find("td:eq(4)").text(); // get current row 5th  cell TD value
+	var col6=$(this).parents("tr").find("td:eq(5)").text(); // get current row 6th  cell  TD value
+	var data=col1+"\n"+col2+"\n"+col3;
+	
+	alert(data);
 
-	var mytable = $('#table');
-	var id = mytable.rows[user.parentNode.parentNode.rowIndex].cells[0].text();
+	// var mytable = $('#table');
+	// var id = mytable.rows[user.parentNode.parentNode.rowIndex].cells[0].text();
 	// alert(id);
 	// alert(user.parentNode.parentNode.rowIndex);
-	globalVariable = id;//retaining the same id of the user and initialized to the first cell
-	// alert(globalVariable);
+	// id = id;//retaining the same id of the user and initialized to the first cell
+	// alert(id);
 	$('#submit').val('Edit');
-	$('#firstname').val(mytable.rows[user.parentNode.parentNode.rowIndex].cells[1].val());
-	$('#lastname').val(mytable.rows[user.parentNode.parentNode.rowIndex].cells[2].text());
-	$('#emailId').val(mytable.rows[user.parentNode.parentNode.rowIndex].cells[3].text());
-	$('#contactno').val(mytable.rows[user.parentNode.parentNode.rowIndex].cells[4].text());
+	inputfname.val(col2);
+	inputlname.val(col3);
+	inputemailid.val(col4);
+	$('#contactno').val(col5);
 	$('#pwd').val("**********");
 	$('#dropdown').val("");
-	// document.getElementById('states').value = "";
-	// document.getElementById('cityname').value = "";
-	// document.getElementById('states').remove();
-	// document.getElementById('cityname').remove();
 	todelete = user.parentNode.parentNode.rowIndex;
-	// alert(todelete);
-	// document.getElementById('submit').value="Edit";
 	bool=true;
 	scrollToTop();
 }
@@ -205,50 +199,53 @@ function editHtmlTbleSelectedRow(rIndex, id) {
 	$('#table').deleteRow(rIndex);
 }
 
+//all the below functions are used to validate each and every fields
+function validateFirstName(element) {//it validates the firstname by length
 
-function validateFirstName(element) {
-
+	var fnameSelector=$('#fnamemessage');
 	if (element.value === "" && element.value.length <= 2) {
-		$('#firstname').css('background-color','red');
-		$('#fnamemessage').text('Not a valid first name');
+		inputfname.css('background-color','red');
+		fnameSelector.text('Not a valid first name');
 	} else {
-		$('#fnamemessage').text('Valid name');
-		$('#firstname').css('background-color','skyblue');
+		fnameSelector.text('Valid name');
+		inputfname.css('background-color','skyblue');
 	}
 }
 
-function validateLastName(element) {
+function validateLastName(element) {//it validates the firstname by length
+	var lnameSelector=$('#lnamemessage');
 	if (element.value === "" && element.value.length <= 2) {
-		$('#lastname').css('background-color','red');
-		$('#lnamemessage').text('Not a valid last name');
+		inputlname.css('background-color','red');
+		lnameSelector.text('Not a valid last name');
 	} else {
-		// document.getElementById('firstname').style.background-color="green";
-		$('#lnamemessage').text('Valid lastname');
-		$('#lastname').css('background-color', 'skyblue');
+		lnameSelector.text('Valid lastname');
+		inputlname.css('background-color','skyblue');
 	}
 }
-
-function validateEmail(element) {
+//validates the email field
+function validateEmail(element) {////it validates the firstname by the regex 
 	var regexEmailPattern = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,10}).([a-z]{2,10}?)$/;
 	var checkEmail = regexEmailPattern.test(element.value);
-	if (!checkEmail) {
-		$('#emailId').css('background-color','red');
-		$('#emailmessage').text('Not a valid EmailId');
+	var emailSelector=$('#emailmessage');
+	if (!checkEmail) {//in case its invalid then background becomes red and message shown as not a validemailid
+		inputemailid.css('background-color','red');
+		emailSelector.text('Not a valid EmailId');
 	} else {
-		$('#emailmessage').text('Valid Email');
-		$('#emailId').css('background-color' ,'skyblue');
+		emailSelector.text('Valid Email');
+		inputemailid.css('background-color' ,'skyblue');
 	}
 }
-
-function validateContact(element) {
+//validates the phone number
+function validateContact(element) {//validates the phone number by length
 	var regexPhonePattern = /^([0-9]{10,11})$/;
 	var value = regexPhonePattern.test(element.value);
+	var contactSelector=$('#contactmessage');
 	if (!value) {
-		$('#contactno').css('background-color','red');
-		$('#contactmessage').text('Not a valid Phone number');
+		inputcontactno.css('background-color','red');
+		contactSelector.text('Not a valid Phone number');
 	} else {
-		$('#contactmessage').text('Valid number');
-		$('#contactno').css('background-color' ,'skyblue');
+		contactSelector.text('Valid number');
+		inputcontactno.css('background-color' ,'skyblue');
 	}
 }
 
@@ -262,9 +259,10 @@ function validatePassword(element) {
 	// 	document.getElementById('firstname').style.background-color="skyblue";
 	// }	
 }
-
+//dropdown is shown based on the selection
 function dropdownfunction() {
 	var country = document.getElementById('dropdown');
+	//first country is selected based on country the states are shown and based on states cities will be shown by calling the thirddropdown() method
 	if (country.value === 'India') {
 		var array = ["Maharashtra", "Delhi"];
 	} else if (country.value === 'Australia') {
@@ -302,10 +300,8 @@ function thirdDropdown(element) {
 	$('#output2').html(concatString);
 }
 function cityvalue(element) {
-	// alert(element.value);
 	city = element.value;
 }
-
 function reloadPage() {
 	window.location.reload();
 }
